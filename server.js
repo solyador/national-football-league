7a7b1389a55bf38b2e59273b5c75e0e463416504
solyador <toufik.mailing@gmail.com>
2018-12-01 20:31:41 +0100

    fix timeout

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ require('./backend/config/dbConfig.js')
 
 app.use(serveStatic(path.join(__dirname, 'frontend/dist')))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/dist/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/frontend/dist/index.html'))
+// })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/dist/index.html'))
+  //res.sendFile(path.join(__dirname+'/frontend/dist/index.html'))
+  res.status(200).send('National footlball league')
 })
 
 app.listen(config.PORT, () => {
